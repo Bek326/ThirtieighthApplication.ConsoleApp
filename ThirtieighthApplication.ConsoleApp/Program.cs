5f6569da09ea53c2daa728802e52636042b2d4af
bekManOfSteal <bekManOfSteal@BEKKING326>
2021-09-29 20:14:23 +0300

    Добавьте файлы проекта.

---
+++ ThirtieighthApplication.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ThirtieighthApplication.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"C:\Users\bekManOfSteal\Downloads\Text1.txt");
+
+            var noPunctuationText = new string(lines.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var words = noPunctuationText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var dict = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                else
+                {
+                    dict.Add(word, 0);
+                }
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
